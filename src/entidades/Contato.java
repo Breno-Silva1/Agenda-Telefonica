1989d6d7314fc44957099c2d506f21562bfff125
Breno-Silva1 <bbreno100@live.com>
2018-03-10 12:23:01 -0300

    Classes modelos

---
+++ src/entidades/Contato.java
@@ -0,0 +1,32 @@
+package entidades;
+
+public class Contato {
+	private String nome;
+	private String email;
+	private String numero;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+}
